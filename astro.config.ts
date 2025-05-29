52e0c3e109fec26e0cfbf09ebb8bf67008df1cf6
frankuxui <fisdray@gmail.com>
2025-05-29 22:28:40 +0200

    initial commit

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	site: import.meta.env.SITE_URL || 'https://frankuxui.dev',
+	site: 'https://frankuxui.dev',
 	adapter: vercel(),
 	env: {
 		schema: {
