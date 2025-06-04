1f3fc7180a544e1275937bcc618f04deb07c5caf
frankuxui <fisdray@gmail.com>
2025-06-04 10:46:17 +0200

    server actions

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,15 @@
+/// <reference path="../.astro/env.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly RESEND_API: string;
+  readonly RESEND_API_KEY: string;
+  readonly EMAIL_FROM: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
 interface Window {
   theme: {
