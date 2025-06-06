bdab462df9fbf363ff7342dadeee232296c4f625
frankuxui <fisdray@gmail.com>
2025-06-06 12:32:02 +0200

    google site verification

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,19 +8,13 @@ export const AUTHOR = 'Frank Esteban'
 export const AUTHOR_NAME = 'Frank Esteban Isdray Junco'
 export const ALTERNATE_NAME = 'Frankuxui'
 
-// Información del sitio
 export const SITE_NAME = 'Frankuxui'
 export const SITE_TITLE = 'Frankuxui - Front-end developer y diseñador UX/UI'
 export const SITE_DESCRIPTION = 'Explora el portafolio de Frank Esteban Isdray Junco, conocido como Frankuxui, desarrollador front-end y diseñador UX/UI en Tarragona con más de 8 años de experiencia.'
-export const KEYWORDS = 'frankuxui, frankuxui portfolio, frankuxui developer, frankuxui designer, frankuxui front-end developer, frankuxui ux/ui designer, frankuxui tarragona, frankuxui españa'
+export const KEYWORDS = 'frankuxui, frankuxui portfolio, frankuxui developer, frankuxui designer, frankuxui front-end developer, frankuxui ux/ui designer, frankuxui tarragona, frankuxui españa, frank esteban, frank, developer, figma, ux/ui, ux, ui'
 
 // URL del sitio
 export const SITE_URL = isDev ? 'http://localhost:4312' : 'https://frankuxui.dev'
-
-// Información de contacto
-export const PHONE = '+34 641932611'
-export const EMAIL = 'fisdray@gmail.com'
-export const PROFESSION = 'Frontend Developer & UX/UI Designer'
 
 // Address
 export const ADDRESS_LOCALITY = 'Salou'
@@ -28,20 +22,9 @@ export const ADDRESS_REGION = 'Tarragona'
 export const POSTAL_CODE = '43840'
 export const ADDRESS_COUNTRY = 'España'
 
-export const PROFILE_FIGMA = 'https://www.figma.com/@frankuxui'
-export const PROFILE_GITHUB = 'https://github.com/frankuxui'
-export const PROFILE_INSTAGRAM = 'https://instagram.com/frankuxui'
-export const PROFILE_LINKEDIN = 'https://www.linkedin.com/in/frankuxui'
-export const PROFILE_FACEBOOK = 'https://www.facebook.com/frankuxui'
-export const PROFILE_CODEPEN = 'https://codepen.io/frankuxui'
-export const PROFILE_CODESANDBOX = 'https://codesandbox.io/u/frankuxui'
-export const PROFILE_DRIBBBLE = 'https://dribbble.com/frankuxui'
-export const PROFILE_TAILWIND_COMPONENTS = 'https://www.creative-tim.com/twcomponents/u/frankuxui'
-export const PROFILE_DEVTO = 'https://dev.to/frankuxui'
-
 export const FRANKUXUI_IMAGE = `${SITE_URL}/frankuxui_frontend_developer_ux_ui_designer.png`
-export const OPENGRAPH_IMAGE = `${SITE_URL}/Opengraph_imagen_de_Frankuxui_desarrollador_frontend_en_Tarragona_Salou.png`
-export const FRANKUXUI_LOGO = `${SITE_URL}/frankuxui_frontend_developer_ux_ui_designer_logo.png`
+export const OG_IMAGE = `${SITE_URL}/Opengraph_imagen_de_Frankuxui_desarrollador_frontend_en_Tarragona_Salou.png`
+export const LOGO = `${SITE_URL}/frankuxui_frontend_developer_ux_ui_designer_logo.png`
 
 
 export const siteConfig = {
@@ -51,11 +34,11 @@ export const siteConfig = {
   siteName: SITE_NAME,
   title: SITE_TITLE,
   description: SITE_DESCRIPTION,
-  profession: PROFESSION,
+  profession: 'Frontend Developer & UX/UI Designer',
   contact: {
-    phone: PHONE,
-    telephone: PHONE,
-    email: EMAIL,
+    phone: '+34 641932611',
+    telephone: '+34 641932611',
+    email: 'fisdray@gmail.com',
   },
   address: {
     locality: ADDRESS_LOCALITY,
@@ -64,16 +47,16 @@ export const siteConfig = {
     country: ADDRESS_COUNTRY,
   },
   social: {
-    figma: PROFILE_FIGMA,
-    github: PROFILE_GITHUB,
-    instagram: PROFILE_INSTAGRAM,
-    linkedin: PROFILE_LINKEDIN,
-    facebook: PROFILE_FACEBOOK,
-    codepen: PROFILE_CODEPEN,
-    codesandbox: PROFILE_CODESANDBOX,
-    dribbble: PROFILE_DRIBBBLE,
-    tailwindComponents: PROFILE_TAILWIND_COMPONENTS,
-    devto: PROFILE_DEVTO,
+    figma: 'https://www.figma.com/@frankuxui',
+    github: 'https://github.com/frankuxui',
+    instagram: 'https://instagram.com/frankuxui',
+    linkedin: 'https://www.linkedin.com/in/frankuxui',
+    facebook: 'https://www.facebook.com/frankuxui',
+    codepen: 'https://codepen.io/frankuxui',
+    codesandbox: 'https://codesandbox.io/u/frankuxui',
+    dribbble: 'https://dribbble.com/frankuxui',
+    tailwindComponents: 'https://www.creative-tim.com/twcomponents/u/frankuxui',
+    devto: 'https://dev.to/frankuxui',
   },
   meta: {
     description: SITE_DESCRIPTION,
@@ -86,7 +69,7 @@ export const siteConfig = {
     siteName: SITE_NAME,
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    image: `${SITE_URL}/Opengraph_imagen_de_Frankuxui_desarrollador_frontend_en_Tarragona_Salou.png`,
+    image: OG_IMAGE,
     images: [
       {
         url: `${SITE_URL}/og-image.png`,
@@ -103,7 +86,7 @@ export const siteConfig = {
     sitemap: `${SITE_URL}/sitemap.xml`,
     robots: `${SITE_URL}/robots.txt`,
   },
-  logo: FRANKUXUI_LOGO,
+  logo: LOGO,
   keywords: KEYWORDS,
 };
 
